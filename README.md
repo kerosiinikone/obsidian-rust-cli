713073047d3cbf95a1c45c3fac4bf9f23694feee
kerosiinikone <onni.ahopelto@gmail.com>
2025-08-13 14:55:13 +0300

    Added a command to create a new note in a configured vault location

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # obsidian-rust-cli
 
-**Quick capture from terminal** -> create a new "idea" note / append to daily notes from a "fleeting thought". Open, create (daily) notes from terminal -> pretty printing. **Incorporate this** -> "Vault Statistics Dashboard"
+**Quick capture from terminal**: create a new "idea" note / append to daily notes from a "fleeting thought". Open, create (daily) notes from terminal, pretty printing. **Incorporate this**: "Vault Statistics Dashboard"
+
+### Workflow (for self)
+
+1. Setup the repo / vault (_config file_)
+   - Read config file into memory before exec (cached?)
+   - Commands to add / edit config from terminal
+   - Give cfg path directly as a **flag**
+2. Commands to create, append, display (_pretty_) and open (_daily_) notes
+   - ...
+
+### Commands (Windows PS)
+
+```powershell
+$Env:VAULT_PATH="env_var"; .\target\debug\obsidian-rust-cli.exe new Hello
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,89 @@
-use clap::Parser;
+use anyhow::Ok;
+use clap::{Parser, Subcommand};
+use std::{env, fs::File, io::Write, path::PathBuf};
 
 #[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    command: Command,
+
+    /// Path to vault
     #[arg(short, long)]
-    example: String,
+    vault: Option<PathBuf>,
 }
 
-fn main() {
+#[derive(Debug, Clone)]
+struct Config {
+    vault: PathBuf,
+    // templating -> similar loading scheme?
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            vault: PathBuf::new(),
+        }
+    }
+}
+
+#[derive(Subcommand, Debug)]
+#[clap(rename_all = "snake_case")]
+enum Command {
+    /// Create a new note from an idea
+    New { idea: String },
+
+    /// Edit the config file (todo!)
+    Config {
+        #[arg(short, long)]
+        vault: Option<PathBuf>,
+
+        #[arg(short, long)]
+        template: Option<PathBuf>,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    eprintln!("{:?}", args.example)
+    // Parse config
+    let mut cfg = Config::new();
+    // Order: flags, env, cfg file, traversion?
+    cfg.vault = if let Some(vault) = args.vault {
+        vault
+    } else if let Result::Ok(vault) = env::var("VAULT_PATH") {
+        PathBuf::from(vault)
+    } else {
+        // TODO: Read cfg file / default to a "default_location"
+        env::current_dir()?
+    };
+
+    eprintln!("{:?}", cfg.vault);
+
+    // Template parsing, etc...
+
+    // Validate the vault location (has to have .obsidian)
+    if !cfg.vault.is_dir() {
+        return Err(anyhow::Error::msg("Invalid vault"));
+    }
+
+    match args.command {
+        Command::New { idea } => {
+            // eprintln!("Idea: {idea}");
+            // eprintln!("Vault: {:?}", cfg.vault);
+
+            // Create a new .md file (with a template, if exists -> parsed earlier)
+            // Name based on timestamp or configured prefixes?
+
+            // Generate path
+            let mut note_path: PathBuf = cfg.vault.clone();
+            note_path.push("test_file.md");
+
+            // Create file
+            let mut handle = File::create(note_path.as_path())?;
+            handle.write_all(idea.as_bytes())?;
+        }
+        _ => return Ok(()),
+    }
+    Ok(())
 }
